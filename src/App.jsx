1f2e73c62846cf0b4f34d17405be87a694c5269d
bidan <bidhan.adhikari@myy.haaga-helia.fi>
2024-10-04 20:36:45 +0300

    Updated list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
 import Todolist from './component/Todolist'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 function App() {
-  
-
-  return (
-    <>
-    <Todolist />
-      
-    </>
-  )
+    return (
+        <Container maxWidth="md">
+            <AppBar position='static'>
+                <Toolbar>
+                    <Typography variant='h6'>
+                        Simple Todo List
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Todolist />
+            <CssBaseline />
+        </Container>
+    )
 }
 
-export default App
+export default App;
--- src/component/Todolist.jsx
+++ src/component/Todolist.jsx
@@ -1,81 +1,101 @@
-import { useState } from "react";
-import TodoTable from './TodoTable';
+import { useState, useRef } from "react";
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the Data Grid
+import Button from '@mui/material/Button';
+import Deleteicon from '@mui/icons-material/Delete';
+import { TextField } from "@mui/material";
+import Stack from '@mui/material/Stack';
 
-function Todolist()  {
-    const [todo, setTodo] = useState( {description: "", duedate: "", priority:""});
-    const [todos, setTodos] = useState([]);
+function Todolist() {
+  const [todo, setTodo] = useState({ description: "", duedate: "", priority: "" });
+  const [todos, setTodos] = useState([]);
+  const gridRef = useRef();
 
-    const [colDefs, setColDefs] = useState([
-        {field: "description", filter:true, },
-        {field: "priority", 
-            cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'} 
-        },
-        {field: "duedate", filter:true}
-    ]);
- 
-    const handleAdd = () => {
-        if(!todo){
-            alert("Type something else")
-        }
-        else {
-            
-            setTodos([todo, ...todos]);
-            setTodo({description: "", duedate: "", priority:""});
+  const [colDefs, setColDefs] = useState([
+    { field: "description", filter: true },
+    {
+      field: "priority",
+      cellStyle: params => params.value === "High" ? { color: 'red' } : { color: 'black' }
+    },
+    { field: "duedate", filter: true }
+  ]);
 
-        }
-    
+  const handleAdd = () => {
+    if (!todo.description || !todo.priority || !todo.duedate) {
+      alert("Please fill in all fields");
+    } else {
+      setTodos([todo, ...todos]);
+      setTodo({ description: "", duedate: "", priority: "" });
     }
-    const handleDelete = (row) => {
-        console.log("Delete:" + row);
-        setTodos(todos.filter((todo, index)=> row!= index));
+  };
+
+  const handleDelete = () => {
+    if (gridRef.current && gridRef.current.getSelectedNodes().length > 0) {
+      const selectedNode = gridRef.current.getSelectedNodes()[0];
+      const rowIndex = selectedNode.rowIndex;
+      setTodos(todos.filter((_, index) => index !== rowIndex));
+    } else {
+      alert('Select a row first!');
     }
-    
-    return(
-        <>
-        <h3> Simple Todolist</h3>
-        <h4> Enter below 👇 </h4>
-        <label> Description:</label>
-          <input
-          type='text'
-          placeholder='Description '
+  };
+
+  return (
+    <>
+
+      <Stack direction="row" spacing={2} alignItems="center"
+       justifyContent="center">
+
+        <TextField
+          type="text"
+          label="Description"
           value={todo.description}
-          onChange={ event => setTodo({...todo, description: event.target.value})}
-          />
-          <label>Priority</label>
-          <input
-          type='text'
-          placeholder='Priority '
-          value={todo.priority}
-          onChange={ event => setTodo({...todo, priority: event.target.value})}
-          />
-          <label> Due Date:</label>
-          <input
-          type='date'
-          placeholder='Due date '
-          value={todo.duedate}
-          onChange={ event => setTodo({...todo, duedate: event.target.value})}
-          />
-          
-          <button onClick={handleAdd}> Add to do</button>
-          <div className='ag-theme-material'
-          style={{height: 500, width: '80% '}}
-          >
-            <AgGridReact
-            rowData={todos}
-            columnDefs={colDefs}
-             rowSelection="single"
-            />
-            
-            
-          </div>
-         
-          </>
-          
-    );
-}
+          onChange={event => setTodo({ ...todo, description: event.target.value })}
+        />
 
+        <TextField
+          type="text"
+          label="Priority"
+          value={todo.priority}
+          onChange={event => setTodo({ ...todo, priority: event.target.value })}
+        />
+
+        <TextField
+          label="Date"
+          placeholder="Due date"
+          value={todo.duedate}
+          onChange={event => setTodo({ ...todo, duedate: event.target.value })}
+        />
+        <Button
+          variant="contained"
+          color="error"
+          onClick={handleAdd}
+          style={{ width: '150px' }}
+
+        >
+          Add to do
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          endIcon={<Deleteicon />}
+          onClick={handleDelete}
+          style={{ width: '150px' }}
+        >
+          Delete
+        </Button>
+      </Stack>
+      <div className='ag-theme-material' style={{ height: 500, width: '80%' }}>
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={params => gridRef.current = params.api}
+          rowData={todos}
+          columnDefs={colDefs}
+          rowSelection="single"
+        />
+      </div>
+    </>
+  );
+}
 
 export default Todolist;
